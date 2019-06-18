9760e6f584a2ac0dc9d5641ef7457b9b156a6f75
MIHIR PATEL <mihirptl89@gmail.com>
2019-06-17 21:12:57 -0500

    fix footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,24 +8,32 @@ const FooterLink = styled.a`
 `
 
 const FooterContainer = styled.footer`
-  display: flex;
-  justify-content: center;
-  text-align: right;
-  width: 100%;
+  margin: auto 0;
   height: 10%;
   bottom: 0;
   left: 0;
   right: 0;
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+const List = styled.ul`
+  margin-left: -20px;
+  text-align: center;
 `
 
 const Footer = () => (
     <FooterContainer>
-    <ul>
+    <List>
         <li><FooterLink href="https://twitter.com/mihirchronicles"><FontAwesomeIcon icon={["fab", "twitter"]} size="2x" style={{color:"#70a2d1"}} /></FooterLink></li>
         <li><FooterLink href="https://www.linkedin.com/in/mihirchronicles"><FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" style={{color:"#70a2d1"}} /></FooterLink></li>
         <li><FooterLink href="https://github.com/mihirchronicles"><FontAwesomeIcon icon={["fab", "github"]} size="2x" style={{color:"#70a2d1"}} /></FooterLink></li>
         <li><FooterLink href="mailto:mihirptl89@gmail.com"><FontAwesomeIcon icon="envelope" size="2x" style={{color:"#70a2d1"}} /></FooterLink></li>
-    </ul>
+    </List>
     </FooterContainer>
 )
 
