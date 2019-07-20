195d29f433f49b954ba1adae74348aa6c05dde03
MIHIR PATEL <mihirptl89@gmail.com>
2019-07-19 22:37:24 -0500

    people

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -19,10 +19,11 @@ const IndexPage = ({ data }) => {
       <SEO title="Explore" />
       <Content>
       <h1>Explore</h1>
-      <Description>“If I have seen further it is by standing on the shoulders of giants.” - Isaac Newton. Sir Isaac Newton knew the trick when he was on a quest for finding gravity. Besides books, I follow these interesting people below. I widen my ears and illuminate my focal point when they have something to say. The only way to climb the hierarchy of rational thinking is to stand on giant shoulders. There are many other giant shoulders that I follow but it would turn into an exhaustive list. Resources below are mostly centered around my interest in investing and philosophy which are in form of shareholder letters and long-form reading posts.</Description>
-        <h2>Letters</h2>
+      <Description>The theme here is to explore by standing tall on the shoulder of giants. “If I have seen further it is by standing on the shoulders of giants.” - Isaac Newton. Sir Isaac Newton knew the trick when he was on a quest for finding gravity. Besides books, I follow these interesting people below. I widen my ears and illuminate my focal point when they have something to say. The only way to climb the hierarchy of rational thinking is to stand on giant shoulders.</Description>
+        <h2>People</h2>
         <ol>
-          <li><a href="http://www.berkshirehathaway.com/">Munger and Buffett</a></li>
+          <h3>Shareholder Letters</h3>
+          <li><a href="http://www.berkshirehathaway.com/">Warren Buffett</a></li>
           <li><a href="https://www.jpmorganchase.com/corporate/investor-relations/annual-report-proxy.htm">Jamie Dimon</a></li>
           <li><a href="https://www.oaktreecapital.com/insights/howard-marks-memos">Howard Marks</a></li>
           <li><a href="https://www.blackrock.com/corporate/investor-relations/larry-fink-chairmans-letter">Larry Fink</a></li>
@@ -31,24 +32,57 @@ const IndexPage = ({ data }) => {
           <li><a href="https://www.csisoftware.com/category/pres-letters/">Mark Leonard</a></li>
           <li><a href="https://papers.ssrn.com/sol3/cf_dev/AbsByAuth.cfm?per_id=77768">Clifford Asness</a></li>
           <li><a href="https://www.socialcapital.com/annual-letters/">Chamath Palihapitiya</a></li>
+          <li><a href="https://www.adventur.es/annual-letters/">Brent Beshore</a></li>
+          <h3>Valuation & Investing</h3>
+          <li><a href="https://aswathdamodaran.blogspot.com/">Aswath Damodaran</a></li>
+          <li><a href="http://michaelmauboussin.com/">Michael J. Mauboussin</a></li>
           <li><a href="https://www.collaborativefund.com/blog/authors/morgan/">Morgan Housel</a></li>
           <li><a href="http://jasonzweig.com/">Jason Zweig</a></li>
           <li><a href="https://25iq.com/">Tren Griffin</a></li>
           <li><a href="https://stratechery.com/">Ben Thompson</a></li>
           <li><a href="http://investorfieldguide.com/">Patrick O’Shaughnessy</a></li>
           <li><a href="http://abovethecrowd.com/">Bill Gurley</a></li>
-          <li><a href="http://michaelmauboussin.com/">Michael J. Mauboussin</a></li>
-          <li><a href="https://www.adventur.es/annual-letters/">Brent Beshore</a></li>
           <li><a href="http://www.philosophicaleconomics.com/">Jessie Livermore (anonymous)</a></li>
           <li><a href="https://www.fooledbyrandomness.com/">Nassim Taleb</a></li>
+          <li><a href="http://www.econtalk.org/">Russell Roberts</a></li>
+          <h3>Startups & Tech</h3>
           <li><a href="http://paulgraham.com/articles.html">Paul Graham</a></li>
-          <li><a href="https://slatestarcodex.com/about/">Scott Alexander</a></li>
-          <li><a href="https://meltingasphalt.com/">Kevin Simler</a></li>
+          <li><a href="https://twitter.com/wolfejosh">Josh Wolfe</a></li>
           <li><a href="https://www.eugenewei.com/">Eugene Wei</a></li>
           <li><a href="http://reactionwheel.net/">Jerry Newmann</a></li>
-          <li><a href="http://worrydream.com/">Bret Victr</a></li>
+          <li><a href="http://cdixon.org/">Chris Dixon</a></li>
+          <li><a href="https://blog.codinghorror.com/">Jeff Atwood</a></li>
           <li><a href="https://patrickcollison.com/">Patrick Collison</a></li>
           <li><a href="https://www.gwern.net/">Gwern Branwen</a></li>
+          <li><a href="https://meltingasphalt.com/">Kevin Simler</a></li>
+          <li><a href="https://twitter.com/jasonfried">Jason Fried</a></li>
+          <li><a href="https://twitter.com/dhh">DHH</a></li>
+          <li><a href="https://www.kalzumeus.com/">Patrick McKenzie</a></li>
+          <li><a href="https://www.perell.com/">David Perell</a></li>
+          <h3>Code</h3>
+          <li><a href="https://twitter.com/mbostock">Mike Bostock</a></li>
+          <li><a href="https://twitter.com/NadiehBremer">Nadieh Bremer</a></li>
+          <li><a href="https://twitter.com/chriscoyier">Chris Coyier</a></li>
+          <li><a href="https://www.andrewng.org/">Andrew Ng</a></li>
+          <li><a href="http://worrydream.com/">Bret Victr</a></li>
+          <li><a href="http://gregbrockman.com/">Greg Brockman</a></li>          
+          <h3>Product</h3>
+          <li><a href="http://juliezhuo.com/">Julie Zhuo</a></li>
+          <li><a href="https://eightshapes.com/nathan-curtis.html">Nathan Curtis</a></li>
+          <li><a href="https://designcode.io">Meng To</a></li>
+          <li><a href="https://www.indiehackers.com/">Courtland Allen</a></li>
+          <li><a href="https://twitter.com/pablostanley">Pablo Stanley</a></li>          
+          <h3>Humanities, Art & Literature</h3>
+          <li><a href="https://slatestarcodex.com/about/">Scott Alexander</a></li>
+          <li><a href="http://swiss-miss.com">Tina Roth Eisenberg</a></li>
+          <li><a href="https://www.brainpickings.org/">Maria Popova</a></li>
+          <li><a href="https://www.oliverjeffers.com/">Oliver Jeffers</a></li>
+          <li><a href="https://boydvarty.com/">Boyd Varty</a></li>
+          <li><a href="http://blas.com">Blas Moros</a></li>
+          <li><a href="https://fs.blog/">Shane Parrish</a></li>
+          <h3>Science</h3>
+          <li><a href="http://siddharthamukherjee.com/">Siddhartha Mukherjee</a></li>
+          <li><a href="https://www.microsoft.com/en-us/research/project/tuva-richard-feynman/">Richard Feynman</a></li>
         </ol>
         <h2>Long Forms</h2>
         <p>Below are also some of the most thought provoking long form articles that I go back to, time and time again.</p>
